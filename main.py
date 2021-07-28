877293527dfcd005c7a8055720718ff410e871e5
edwardzhang5 <edwardzhang5@gmail.com>
2021-07-27 23:22:11 -0400

    installed some packages, pyalgotrade

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 # Author: Edward Zhang
 # I don't know what this does yet
 
-print("hello world")
+# Import Packages
+
+import numpy as np
+import pandas as pd
+import pyfolio as pf
+
+stock_rets = pf.utils.get_symbol_rets('FB')
+pf.create_returns_tear_sheet(stock_rets, live_start_date='2015-12-1')
